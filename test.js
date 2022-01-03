548b655f1d6916132946086b548fb6f44ad92cb1
sinmasa <sinmasa@gmail.com>
2022-01-03 17:37:00 +0900

    建物と地面を追加。３D建物オブジェクトはうまく行かないので一旦コメントアウト。

--- test.js
+++ test.js
@@ -1,11 +1,28 @@
 
   window.addEventListener('DOMContentLoaded', init);
 
+  var v = 0; // 速度
+
+  const KEYCODE_UP = 38;
+  const KEYCODE_DN = 40;
+
+  window.addEventListener("keydown", event => {
+    console.log("keyCode= "+event.keyCode+"¥n");
+    if (event.keyCode === KEYCODE_DN) {
+      v += 1;
+    }
+    if (event.keyCode === KEYCODE_UP) {
+      v -= 1;
+    }
+    console.log("速度= "+v+"¥n");
+  });
 
   function init() {
     const width = 960;
     const height = 540;
   
+    // const fbx = new FBXLoader();
+
     // レンダラーを作成
     const renderer = new THREE.WebGLRenderer({
       canvas: document.querySelector('#myCanvas')
@@ -23,56 +40,48 @@
     camera.position.set(0, 0, 0);
     //camera.far = 10000;
   
-    // スプライト(遠景)
-    const spSora = new THREE.Sprite(new THREE.SpriteMaterial({
-      map: new THREE.TextureLoader().load('img/sora.jpg'),
-    }));
-    spSora.scale.set(1920*20, 1271*8);
-    spSora.position.set(0,0,-9000);
-    //scene.add(spSora);
-
     // スプライト（運転席）
     const sppUnten = new THREE.Sprite(new THREE.SpriteMaterial({
       map: new THREE.TextureLoader().load('img/unten.svg'),
 //      map: new THREE.TextureLoader().load('img/syasyou.png'),
     }));
     sppUnten.scale.set(500, 180);
-    sppUnten.position.set(0,-130,700);
+    sppUnten.position.set(0,130,700);
     scene.add(sppUnten);
-
-    // 箱を作成
-    //const geometry = new THREE.BoxGeometry(500, 500, 500);
-    const geometry = new THREE.BoxGeometry(200, 200, 200);
-    const material = new THREE.MeshStandardMaterial({color: 0xFF00FF});
-    const box = new THREE.Mesh(geometry, material);
-    scene.add(box);
-
-    box.position.set(500, 0, -5000);
-
-    const box2 = new THREE.Mesh(new THREE.BoxGeometry(200, 200, 200),  new THREE.MeshStandardMaterial({color: 0xFF00FF}));
-    scene.add(box2);
-
-    box2.position.set(-500, 0, -2000);
-
+    
     var x = 0;
     var z = 0;
     var rot = 0;
-  // 線路を作成
-    for (var i = 0; i < 500; i++) {
+
+    for (var i = 0; i < 1000; i++) {
+      // 地面
+      const ground = new THREE.Mesh(new THREE.BoxGeometry(10000, 1, 1000),  new THREE.MeshStandardMaterial({color: (0x888888)}));
+      scene.add(ground);
+
+      // 線路を作成
       const rail = new THREE.Mesh(new THREE.BoxGeometry(800,10, 1000), new THREE.MeshStandardMaterial({
         map: new THREE.TextureLoader().load('img/rail.jpg'),
         side: THREE.DoubleSide,
       }));
+      scene.add(rail);
       //rot = Math.PI * (0.001 * i) * 5;
       rot = 0;
       x -= Math.sin(rot) * 1000;
       z -= Math.cos(rot) * 1000;
       rail.position.set(x, -300, z);
       rail.rotation.y = rot;
-      scene.add(rail);
       //x += 1;
       //z -= 1000;
+      ground.position.set(x, -300, z);
+
+      // 建物を作成
+      for (var j = 0; j < 3; j++) {
+        const box = new THREE.Mesh(new THREE.BoxGeometry((150 + (Math.random() * 300)), (300 + (Math.random() * 1000)), (150 + (Math.random() * 300))),  new THREE.MeshStandardMaterial({color: (0x000000 + (Math.random() * 0xFFFFFF))}));
+        scene.add(box);
+        box.position.set((Math.floor(Math.random() * 2) > 0 ? 1 : -1) * (600 + (Math.random() * 2000)), -300, z - ( (j * 1000 / 3) + (Math.random() * 200) - 100));
+      }
     }
+
     // 平行光源
     const ambLight = new THREE.AmbientLight(0x777777);
     //ambLight.intensity = 0.3; 
@@ -81,9 +90,14 @@
     const light = new THREE.DirectionalLight(0xFFFFFF);
     //const light = new THREE.AmbientLight(0xFFFFFF);
     light.intensity = 0.1; // 光の強さを倍に
-    light.position.set(1,1000,-8000);
+    light.position.set(0,50000,0);
     // シーンに追加
     scene.add(light);
+
+    // // 建物
+    // fbx.load('img/buildings/building.fbx', (obj)=>{
+    //   scene.add(obj);
+    // });
 
     // フォグを設定
     // new THREE.Fog(色, 開始距離, 終点距離);
@@ -97,22 +111,6 @@
     function tick() {
       requestAnimationFrame(tick);
   
-      // // 箱を回転させる
-      // box.rotation.x += 0.01;
-      // box.rotation.y += 0.01;
-      // box.rotation.z += 0.08;
-
-      // if (! box.dir) {
-      //   box.dir = 1;
-      // } else if (box.position.x > 300) {
-      //   box.dir = -1;
-      // } else if (box.position.x < -300) {
-      //   box.dir = 1;
-      // }
-      // box.position.x += box.dir * 10;
-
-      const v = 200;
-
       var cx = 0.0;
       var cz = 0.0;
       var rot = 0.0;
@@ -136,15 +134,12 @@
       cx -= Math.sin(rot) * (Math.abs(len) % 1000);
       cz -= Math.cos(rot) * (Math.abs(len) % 1000);
 
-      //document.getElementById("dbg").innerText = 'len=' + len + ', x=' + cx + ', z=' + cz;
+      document.getElementById("dbg").innerText = '速度=' + Math.abs(v) + ', 距離=' + len;
 
       camera.rotation.y = rot;
       camera.position.x = cx;
       camera.position.y = 0;
       camera.position.z = cz;
-
-      spSora.position.x = cx;
-      spSora.position.z = cz - 9000;
 
       sppUnten.position.x = cx;
       sppUnten.position.z = cz - 270;
@@ -155,8 +150,3 @@
     }
   }
   
-function test()
-{
-    var n = Math.floor(Math.random()*30);
-    alert('test!' + n);
-}
